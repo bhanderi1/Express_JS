6f94630144136e18280a4f19bbff6f5c04dbbf37
Shurshti Bhanderi <srushtibhanderi77@gmail.com>
2024-08-24 18:37:10 +0530

    Lecture-14

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// ******************* Lecture-14 (image Uploading using thrid party library => multer)  ***********************
+// ******************* Lecture-15 (tem)  ***********************
 require("dotenv").config();
 const express = require('express')
 const morgan = require('morgan')
@@ -7,12 +7,15 @@ const mongoose = require("mongoose")
 const userRoutes = require('./routes/user.routes')
 const productRoutes = require('./routes/product.routes')
 const port = process.env.PORT;
+const cors = require('cors')
+const path = require('path')
 
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use("/public/images",express.static(path.join(__dirname, "public/images")))
 
 app.get("/", (req, res) => {
   res.send("welcome to express server")
